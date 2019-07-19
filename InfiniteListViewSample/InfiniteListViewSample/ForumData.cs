13ac79a695ef114c378f0286ebb088763040cf6e
Aswin P G <aswinpg@outlook.in>
2019-07-19 16:06:09 +0530

    Add project files.

---
+++ InfiniteListViewSample/InfiniteListViewSample/ForumData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteListViewSample
+{
+    public class ForumData
+    {
+        public string Tag { get; set; }
+        public string Heading { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public string Name { get; set; }
+        public string PostedOn { get; set; }
+
+    }
+}
---
+++ InfiniteListViewSample/InfiniteListViewSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Extended;
+
+namespace InfiniteListViewSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        private const int PageSize = 20;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Items = new InfiniteScrollCollection<ForumData>
+            {
+                OnLoadMore = async () =>
+                {
+                    // load the next page
+                    var page = Items.Count / PageSize;
+                    var items = await dataSource.GetItemsAsync(page + 1, PageSize);
+
+                    // return the items that need to be added
+                    return items;
+                }
+            };
+
+        }
+        public InfiniteScrollCollection<ForumData> Items { get; }
+    }
+}
